7992c4a421b4b9779caffc5b1c796f8a258ced9e
Alcaventas <141533784+Alcaventas@users.noreply.github.com>
2024-04-15 21:13:44 -0600

    Actualizar gc-setdesc.js

--- plugins/gc-setdesc.js
+++ plugins/gc-setdesc.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn, args}) => {
   await conn.groupUpdateDescription(m.chat, `${args.join(' ')}`);
-  m.reply('*✅ La descripción del grupo se modifico correctamente*');
+  m.reply('*✅ 𝙎𝙀 𝙈𝙊𝘿𝙄𝙁𝙄𝘾𝙊 𝘾𝙊𝙉 𝙀𝙓𝙄𝙏𝙊 𝙇𝘼 𝘿𝙀𝙎𝘾𝙍𝙄𝙋𝘾𝙄𝙊𝙉 𝘿𝙀𝙇 𝙂𝙍𝙐𝙋𝙊*');
 };
 handler.help = ['Setdesc <text>'];
 handler.tags = ['group'];
